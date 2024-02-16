0a9125b96284dd65c8b30e21dbc63abaccbcd633
Cauã Diório <caua.diorio29@gmail.com>
2024-02-16 16:47:19 -0300

    atividade finalizada

--- AppVendasWeb/AppVendasWeb/Controllers/CategoriasController.cs
+++ AppVendasWeb/AppVendasWeb/Controllers/CategoriasController.cs
@@ -10,149 +10,149 @@ using AppVendasWeb.Models;
 
 namespace AppVendasWeb.Controllers
 {
-    public class CategoriasController : Controller
-    {
-        private readonly AppVendasContext _context;
+	public class CategoriasController : Controller
+	{
+		private readonly AppVendasContext _context;
 
-        public CategoriasController(AppVendasContext context)
-        {
-            _context = context;
-        }
+		public CategoriasController(AppVendasContext context)
+		{
+			_context = context;
+		}
 
-        // GET: Categorias
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Categorias.ToListAsync());
-        }
+		// GET: Categorias
+		public async Task<IActionResult> Index()
+		{
+			return View(await _context.Categorias.ToListAsync());
+		}
 
-        // GET: Categorias/Details/5
-        public async Task<IActionResult> Details(Guid? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		// GET: Categorias/Details/5
+		public async Task<IActionResult> Details(Guid? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var categoria = await _context.Categorias
-                .FirstOrDefaultAsync(m => m.CategoriaId == id);
-            if (categoria == null)
-            {
-                return NotFound();
-            }
+			var categoria = await _context.Categorias
+				.FirstOrDefaultAsync(m => m.CategoriaId == id);
+			if (categoria == null)
+			{
+				return NotFound();
+			}
 
-            return View(categoria);
-        }
+			return View(categoria);
+		}
 
-        // GET: Categorias/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
+		// GET: Categorias/Create
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-        // POST: Categorias/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("CategoriaId,CategoriaNome")] Categoria categoria)
-        {
-            if (ModelState.IsValid)
-            {
-                categoria.CategoriaId = Guid.NewGuid();
-                _context.Add(categoria);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(categoria);
-        }
+		// POST: Categorias/Create
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("CategoriaId,CategoriaNome")] Categoria categoria)
+		{
+			if (ModelState.IsValid)
+			{
+				categoria.CategoriaId = Guid.NewGuid();
+				_context.Add(categoria);
+				await _context.SaveChangesAsync();
+				return RedirectToAction(nameof(Index));
+			}
+			return View(categoria);
+		}
 
-        // GET: Categorias/Edit/5
-        public async Task<IActionResult> Edit(Guid? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		// GET: Categorias/Edit/5
+		public async Task<IActionResult> Edit(Guid? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var categoria = await _context.Categorias.FindAsync(id);
-            if (categoria == null)
-            {
-                return NotFound();
-            }
-            return View(categoria);
-        }
+			var categoria = await _context.Categorias.FindAsync(id);
+			if (categoria == null)
+			{
+				return NotFound();
+			}
+			return View(categoria);
+		}
 
-        // POST: Categorias/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Guid id, [Bind("CategoriaId,CategoriaNome")] Categoria categoria)
-        {
-            if (id != categoria.CategoriaId)
-            {
-                return NotFound();
-            }
+		// POST: Categorias/Edit/5
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(Guid id, [Bind("CategoriaId,CategoriaNome")] Categoria categoria)
+		{
+			if (id != categoria.CategoriaId)
+			{
+				return NotFound();
+			}
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(categoria);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CategoriaExists(categoria.CategoriaId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(categoria);
-        }
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					_context.Update(categoria);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!CategoriaExists(categoria.CategoriaId))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			return View(categoria);
+		}
 
-        // GET: Categorias/Delete/5
-        public async Task<IActionResult> Delete(Guid? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		// GET: Categorias/Delete/5
+		public async Task<IActionResult> Delete(Guid? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var categoria = await _context.Categorias
-                .FirstOrDefaultAsync(m => m.CategoriaId == id);
-            if (categoria == null)
-            {
-                return NotFound();
-            }
+			var categoria = await _context.Categorias
+				.FirstOrDefaultAsync(m => m.CategoriaId == id);
+			if (categoria == null)
+			{
+				return NotFound();
+			}
 
-            return View(categoria);
-        }
+			return View(categoria);
+		}
 
-        // POST: Categorias/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(Guid id)
-        {
-            var categoria = await _context.Categorias.FindAsync(id);
-            if (categoria != null)
-            {
-                _context.Categorias.Remove(categoria);
-            }
+		// POST: Categorias/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(Guid id)
+		{
+			var categoria = await _context.Categorias.FindAsync(id);
+			if (categoria != null)
+			{
+				_context.Categorias.Remove(categoria);
+			}
 
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
 
-        private bool CategoriaExists(Guid id)
-        {
-            return _context.Categorias.Any(e => e.CategoriaId == id);
-        }
-    }
+		private bool CategoriaExists(Guid id)
+		{
+			return _context.Categorias.Any(e => e.CategoriaId == id);
+		}
+	}
 }
